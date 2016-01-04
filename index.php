47b5a24fd75b1a767897e99a617247c9a8b59558
Juanjo <juanjo_23@live.com>
2016-01-04 17:57:54 -0600

    changes in the github link

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
 	 				</a>
 	 			</td>
 	 			<td>
-	 				<a href="https://github.com/juanjo23" target="_blank">
+	 				<a href="https://github.com/" target="_blank">
 	 					<img src="images/octocat.png">
 	 				</a>
 	 			</td>
